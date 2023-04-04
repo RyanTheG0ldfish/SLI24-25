35366de24f5027ab79884d5909e6c68f2104d1d5
Sean Radke <sradke2035@gmail.com>
2023-04-04 11:07:50 -0500

    HardwareSerial for GPS

--- Deployment/src/main.cpp
+++ Deployment/src/main.cpp
@@ -9,22 +9,9 @@
 #include <RHReliableDatagram.h>
 #include <Servo.h>
 
-// GPS
-static const uint32_t GPSBaud = 9600;   //GPS Baud rate
-TinyGPSPlus gps;   //GPS Driver
-static const int RXPin = 0, TXPin = 1;  //This defines the pins that are being used for RX and TX (UART)
-SoftwareSerial ss(RXPin, TXPin);    //Shouldn't need to be using Software Serial - am on hardwhere serial pins.
-
-// Radio
-#define CLIENT_ADDRESS 1
-#define SERVER_ADDRESS 2
-RH_RF95 rf95(3, 2);        // Singleton instance of the radio driver
-
-// BMP 3XX
-Adafruit_BMP3XX bmp = Adafruit_BMP3XX(); //Initializes the driver
-#define BMP_SCK 13      //Clock Pin
-#define BMP_MISO 12     //MISO (Motherboard In - Signal Out)
-#define BMP_MOSI 11     //MOSI (Motherboard Out - Signal In)
+TinyGPSPlus gps;    //GPS Driver
+RH_RF95 rf95(3, 2); // Singleton instance of the radio driver
+Adafruit_BMP3XX bmp = Adafruit_BMP3XX(); //Initializes the bmp3xx driver
 #define BMP_CS 4        //Chip Select Pin
 #define SEALEVELPRESSURE_HPA (1013.25)  //Sea Level Pressure
 
@@ -32,7 +19,6 @@ Adafruit_BMP3XX bmp = Adafruit_BMP3XX(); //Initializes the driver
 AccelStepper motor1(1, 8, 9);         //(1, 8, 9) where 1 = driver board, 8 = pinStep, and 9 = pinDirection
 AccelStepper motor2(1, 14, 15);       //Defining related pins for the motor driver
 
-// Untested Servo Control
 Servo servo1; // create servo object #1 to control servo 1
 Servo servo2; // create servo object #2 to control servo 2
 
@@ -45,17 +31,13 @@ void setup()
 {
     // Serial
     Serial.begin(9600);
-
-    // GPS
-    ss.begin(GPSBaud);
+    Serial1.begin(9600);// GPS
 
     // Radio
     if (!rf95.init())
         Serial.println("Radio init failed"); // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on...The default transmitter power is 13dBm, using PA_BOOST.
-    // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then you can set transmitter powers from 2 to 20 dBm:
-    //  driver.setTxPower(20, false);
-    // You can optionally require this module to wait until Channel Activity Detection shows no activity on the channel before transmitting by setting the CAD timeout to non-zero:
-    //  driver.setCADTimeout(10000);
+      //rf95.setModemConfig(RH_RF95::Bw500Cr45Sf128); // You can change the modulation parameters with eg
+      //rf95.setTxPower(20, false); //The default transmitter power is 13dBm, using PA_BOOST. - If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then you can set transmitter powers from 2 to 20 dBm:
 
     // BMP 3XX
     if (!bmp.begin_SPI(BMP_CS)) // hardware SPI mode
@@ -67,7 +49,7 @@ void setup()
     bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
     bmp.setOutputDataRate(BMP3_ODR_50_HZ);
 
-    // Stepper Motors
+  // Stepper Motors
   motor1.setMaxSpeed(100);
   motor1.setAcceleration(10);
   motor2.setMaxSpeed(100);
@@ -88,8 +70,10 @@ void setup()
 
 void loop()
 {
-    while (ss.available() > 0) { //GPS
-      gps.encode(ss.read());  }
+     while (Serial1.available() > 0)
+    {
+        gps.encode(Serial1.read());
+    }
     
     bmp.performReading(); //Altimeter
 
@@ -157,66 +141,3 @@ void loop()
   motor1.run(); //run the stepper
   motor2.run(); //run the stepper
 }
-
-/* ENVISIONED FLOW PATTERN OF THIS CODE IN REAL LIFE ----------------------------------------------------------------------------------------------
-2) Establish radio communication on the ground with handheld controller - Handheld controller receives the green light
-3) Wait for launch to complete - May lose communication halfway through flight as we exit our antennae range
-4) Landed - Regained Communication between controller (Wait for command to be received)
-5) Receive "Separate" command from handheld controller
-6) Power Stepper motors from Position X to Position Y
-7) Send "Success" Message to handheld controller for successful separation
-9) Send "Success" Message for correct UAV Startup communication (radio established and GPS communication established)
-7) Wait for command from handheld controller
-8) Receive "LAUNCH" command from handheld controller
-9) Move servo motors from position X to position Y
-10) Send "Launch" Command to UAV with position data of the Rocket
-11) UAV takes off - This codes mission is ALMOST done.
-12) Continue sending GPS coordinate and altitude data to UAV WHEN REQUESTED by the UAV.
-13) STOP sending data after UAV has landed and "STOP" command is given.
-*/
-
-/* SERVER SIDE RF95
-     Serial.println("Sending to rf95_reliable_datagram_server");
-
-    if (manager.available())
-      {
-        // Wait for a message addressed to us from the client
-        uint8_t len = sizeof(buf);
-        uint8_t from;
-        if (manager.recvfromAck(buf, &len, &from))
-        {
-          Serial.print("got request from : 0x");
-          Serial.print(from, HEX);
-          Serial.print(": ");
-          Serial.println((char*)buf);
-
-          // Send a reply back to the originator client
-          if (!manager.sendtoWait(data, sizeof(data), from))
-            Serial.println("sendtoWait failed");
-        }
-      }
-      */
-
-    /*      CLIENT SIDE RF95
-    // Send a message to manager_server
-    if (manager.sendtoWait(data, sizeof(data), SERVER_ADDRESS))
-    {
-      // Now wait for a reply from the server
-      uint8_t len = sizeof(buf);
-      uint8_t from;
-      if (manager.recvfromAckTimeout(buf, &len, 2000, &from))
-      {
-        Serial.print("got reply from : 0x");
-        Serial.print(from, HEX);
-        Serial.print(": ");
-        Serial.println((char*)buf);
-      }
-      else
-      {
-        Serial.println("No reply, is rf95_reliable_datagram_server running?");
-      }
-    }
-    else
-      Serial.println("sendtoWait failed");
-    delay(500);
-    */
